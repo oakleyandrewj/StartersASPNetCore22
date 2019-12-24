0cd39d6e5d16ffb90cbf78a82c61c4b012e6df29
Andy Oakley <aoa@stephensons.co.uk>
2019-12-24 16:06:00 +0000

    Added Starter Model

---
+++ StartersAdminSystem/StartersAdminSystem/Models/Starter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StartersAdminSystem.Models
+{
+    public class Starter
+    {
+        public int Id { get; set; }
+        public string Forename { get; set; }
+        public string Surname { get; set; }
+        public string Initials { get; set; }
+        public decimal Salary { get; set; }
+        public string ImageUrl { get; set; }
+        public string ImageThumbnailUrl { get; set; }
+        public DateTime StartDate { get; set; }
+        public string Department { get; set; }
+        public string Office { get; set; }
+        public string Ddi { get; set; }
+
+    }
+}
