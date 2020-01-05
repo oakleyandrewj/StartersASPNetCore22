979e0036ee4dc4074732d4ceb20718ff69cbdf33
Andy Oakley <aoa@stephensons.co.uk>
2020-01-05 17:24:08 +0000

    added controllers folder added home controller, with constructor injection

---
+++ StartersAdminSystem/StartersAdminSystem/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using StartersAdminSystem.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace StartersAdminSystem.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IStarterRepository _StarterRepository;
+
+        public HomeController(IStarterRepository starterRepository)
+        {
+            _StarterRepository = starterRepository;
+        }
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
